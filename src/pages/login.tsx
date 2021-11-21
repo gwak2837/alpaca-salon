c1eae77aee69448397050b538e63dd975b9caccf
Taeuk Gwak <gwak2837@kakao.com>
2021-11-21 12:53:49 +0900

    Update login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,29 +1,39 @@
 import Image from 'next/image'
 import { useRouter } from 'next/router'
+import React from 'react'
 import PageHead from 'src/components/PageHead'
 import { ALPACA_SALON_BACKGROUND_COLOR, ALPACA_SALON_COLOR } from 'src/models/constants'
+import { SquareFrame } from 'src/styles'
 import styled from 'styled-components'
 
 import KakaoIcon from '../svgs/kakao-icon.svg'
+import { FlexContainerColumnEnd } from './[userUniqueName]'
 
-const Padding = styled.div`
-  padding: 1rem;
-  background: #fcfcfc;
-  height: 100vh;
-  max-width: 400px;
-  margin: 0 auto;
-`
-
-const Text = styled.div`
-  padding: 1.5rem;
-  background: ${ALPACA_SALON_BACKGROUND_COLOR};
-  line-height: 2rem;
-`
-
-const PrimaryColorText = styled.span`
-  color: ${ALPACA_SALON_COLOR};
-  font-size: 1.3rem;
+const H2 = styled.h2`
   font-weight: 600;
+`
+
+const H4 = styled.h4`
+  font-weight: 600;
+`
+
+const H5 = styled.h5`
+  color: #676767;
+  padding: 1.5rem 0;
+  text-align: center;
+`
+
+const GridContainerTemplate = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 3fr 1fr;
+  grid-template-rows: 1fr 3.5fr 1fr;
+
+  > div {
+    grid-column: 2 / 3;
+    grid-row: 2 / 3;
+    width: 100%; // for safari
+    cursor: pointer;
+  }
 `
 
 const KakaoButton = styled.div`
@@ -36,6 +46,7 @@ const KakaoButton = styled.div`
   width: 100%;
   background: #fee500;
   padding: 1rem;
+  margin: 0 0 1rem;
   transition: background 0.3s ease-in;
   border-radius: 10px;
 
@@ -51,6 +62,30 @@ const KakaoButton = styled.div`
   }
 `
 
+const Padding = styled.div`
+  display: flex;
+  flex-flow: column;
+  height: 100vh;
+  padding: 2rem 1rem 0;
+
+  > :last-child {
+    flex-grow: 1;
+  }
+`
+
+const PrimaryColorText = styled.span`
+  color: ${ALPACA_SALON_COLOR};
+  font-size: 1.3rem;
+  font-weight: 500;
+`
+
+const Text = styled.div`
+  background: ${ALPACA_SALON_BACKGROUND_COLOR};
+  border-radius: 10px;
+  line-height: 2rem;
+  padding: 1.5rem;
+`
+
 const description = '알파카살롱에 로그인하세요'
 
 export default function LoginPage() {
@@ -59,31 +94,40 @@ export default function LoginPage() {
   return (
     <PageHead title="로그인 - 알파카살롱" description={description}>
       <Padding>
-        <h3>
-          당당하게 더 멋진 인생을 살고 싶은
-          <br />
-          멋쟁이 알파카님, 안녕하세요
-        </h3>
+        <H4>당당하게 더 멋진 인생을 살고 싶은</H4>
+        <br />
+        <H2>멋쟁이 알파카님, 안녕하세요</H2>
 
-        <Image src="/images/login-alpaca.png" alt="login alpaca" width="200" height="200" />
+        <GridContainerTemplate>
+          <SquareFrame>
+            <Image
+              src="/images/login-alpaca.png"
+              alt="login alpaca"
+              layout="fill"
+              objectFit="cover"
+            />
+          </SquareFrame>
+        </GridContainerTemplate>
 
         <Text>
           알파카살롱은 <br />
           <PrimaryColorText>40대 이상 여성</PrimaryColorText> 에게만 오픈된 공간이에요.
         </Text>
 
-        <h5>카카오톡 로그인 혹은 회원가입으로 여성임을 확인해 주세요.</h5>
+        <FlexContainerColumnEnd>
+          <H5>카카오 로그인으로 40대 이상 여성임을 확인해 주세요</H5>
 
-        <KakaoButton
-          onClick={() =>
-            router.push(
-              `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_BACKEND_URL}/oauth/kakao`
-            )
-          }
-        >
-          <KakaoIcon />
-          카카오 로그인
-        </KakaoButton>
+          <KakaoButton
+            onClick={() =>
+              router.push(
+                `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_BACKEND_URL}/oauth/kakao`
+              )
+            }
+          >
+            <KakaoIcon />
+            카카오 로그인
+          </KakaoButton>
+        </FlexContainerColumnEnd>
       </Padding>
     </PageHead>
   )
