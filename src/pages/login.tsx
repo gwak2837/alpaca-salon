291c6b50d91bfe3956516958612787fc634dc8a0
곽태욱 <gwak2837@kakao.com>
2021-12-18 14:20:59 +0900

    Remove login page in brower history

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -85,7 +85,9 @@ const description = '알파카살롱에 로그인하세요'
 
 export default function LoginPage() {
   function goToKakaoLoginPage() {
-    window.location.href = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_BACKEND_URL}/oauth/kakao`
+    window.location.replace(
+      `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_BACKEND_URL}/oauth/kakao`
+    )
   }
 
   return (
